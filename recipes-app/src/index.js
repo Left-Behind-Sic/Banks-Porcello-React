3f4a2d48fa585860408da80986a81c5cf38bf1af
Left-Behind-Sic <95462486+Left-Behind-Sic@users.noreply.github.com>
2022-05-20 15:15:33 +0500

    page 146

--- recipes-app/src/index.js
+++ recipes-app/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import colors from "./data/color-data.json";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export const ColorContext = createContext();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ColorContext.Provider value={{ colors }}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </ColorContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
